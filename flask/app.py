5364676a00d1daf4baf2a4382bac5cfa9ff7dba7
dorhen2 <80155039+dorhen2@users.noreply.github.com>
2021-05-24 13:05:10 +0300

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = '123'
+
 prog = ["Java", "Python"]
 level = ["Professional", "Beginner"]
 
@@ -8,6 +10,7 @@ level = ["Professional", "Beginner"]
 @app.route('/')
 def hello_world():
     return render_template('cv.html')
+
 
 @app.route('/hi')
 def assignment():
@@ -22,17 +25,20 @@ def assignment():
 def colleagues():
     return render_template('Colleague.html')
 
+
 @app.route('/contact')
 def Contacts():
     return render_template('Contact.html')
 
+
 @app.route('/assignmen8')
 def assignmentPage():
     return render_template(
-                           user={'firstName': "", 'gender': "Ms"},
-                           len=len(prog),
-                           prog=prog,
-                           level=level)
+        user={'firstName': "", 'gender': "Ms"},
+        len=len(prog),
+        prog=prog,
+        level=level)
+
 
 @app.route('/skills')
 def my_skill():
@@ -42,6 +48,34 @@ def my_skill():
                            prog=prog,
                            level=level)
 
+
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9():
+    if 'userFirst' in session:
+        userFirst, userLast = session['userFirst'], session['userLast']
+    else:
+        if 'userFirst' in request.args:
+            userFirst = request.args['userFirst']
+            session['userFirst'] = userFirst
+        else:
+            userFirst = ''
+        if 'userLast' in request.args:
+            userLast = request.args['userLast']
+            session['userLast'] = userLast
+        else:
+            userLast = ''
+
+    if 'role' in request.args:
+        role = request.args['role']
+    else:
+        role = ''
+    if 'logOut' in request.args:
+        userFirst = ''
+
+    return render_template('assignment9.html',
+                           role=role,
+                           userFirst=userFirst,
+                           userLast=userLast)
 
 
 if __name__ == '__main__':
